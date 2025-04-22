12af82901046ce5dae0182049489d84e584b32fe
HTRMC <htrmcox@outlook.com>
2025-04-22 11:21:04 +0200

    fix(core): Add proper header guards to windows_wrapper.hpp

--- src/core/windows_wrapper.hpp
+++ src/core/windows_wrapper.hpp
@@ -1,6 +1,12 @@
 // windows_wrapper.h
 #pragma once
+
+// Only define NOMINMAX if it hasn't been defined already
+#if !defined(NOMINMAX)
 #define NOMINMAX
+#endif
+
 #include <windows.h>
+
 #undef ERROR
 #undef WARN
