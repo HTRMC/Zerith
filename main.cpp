c7ab3c314b60425ccab8bf36d57c6641e933c3ba
HTRMC <htrmcox@outlook.com>
2025-05-22 20:54:04 +0200

    Removed unused GLM quaternion includes - Cleaned up #include <glm/gtc/quaternion.hpp> and #include <glm/gtx/quaternion.hpp> from main.cpp since quaternions are no longer used

--- main.cpp
+++ main.cpp
@@ -4,8 +4,6 @@
 #define GLM_ENABLE_EXPERIMENTAL
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/quaternion.hpp>
-#include <glm/gtx/quaternion.hpp>
 
 #include <iostream>
 #include <stdexcept>
