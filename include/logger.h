d17d2eb00210a57f7ce828997f248adc9bd3e3de
HTRMC <htrmcox@outlook.com>
2025-06-21 21:36:33 +0200

    Update logger.h

--- include/logger.h
+++ include/logger.h
@@ -5,6 +5,8 @@
 #include <spdlog/sinks/basic_file_sink.h>
 #include <spdlog/sinks/rotating_file_sink.h>
 #include <memory>
+#include <string>
+#include <cstdio>
 
 class Logger {
 public:
@@ -27,6 +29,16 @@ public:
     void shutdown();
     
     std::shared_ptr<spdlog::logger> getLogger() { return logger_; }
+    
+    // Printf-style logging helpers
+    template<typename... Args>
+    void logPrintf(spdlog::level::level_enum level, const char* format, Args... args) {
+        if (logger_->should_log(level)) {
+            char buffer[4096];
+            std::snprintf(buffer, sizeof(buffer), format, args...);
+            logger_->log(level, buffer);
+        }
+    }
 
 private:
     Logger();
@@ -37,12 +49,12 @@ private:
     std::shared_ptr<spdlog::sinks::rotating_file_sink_mt> file_sink_;
 };
 
-#define LOG_FATAL(...) Logger::getInstance().getLogger()->critical(__VA_ARGS__)
-#define LOG_ERROR(...) Logger::getInstance().getLogger()->error(__VA_ARGS__)
-#define LOG_WARN(...)  Logger::getInstance().getLogger()->warn(__VA_ARGS__)
-#define LOG_INFO(...)  Logger::getInstance().getLogger()->info(__VA_ARGS__)
-#define LOG_DEBUG(...) Logger::getInstance().getLogger()->debug(__VA_ARGS__)
-#define LOG_TRACE(...) Logger::getInstance().getLogger()->trace(__VA_ARGS__)
+#define LOG_FATAL(...) Logger::getInstance().logPrintf(spdlog::level::critical, __VA_ARGS__)
+#define LOG_ERROR(...) Logger::getInstance().logPrintf(spdlog::level::err, __VA_ARGS__)
+#define LOG_WARN(...)  Logger::getInstance().logPrintf(spdlog::level::warn, __VA_ARGS__)
+#define LOG_INFO(...)  Logger::getInstance().logPrintf(spdlog::level::info, __VA_ARGS__)
+#define LOG_DEBUG(...) Logger::getInstance().logPrintf(spdlog::level::debug, __VA_ARGS__)
+#define LOG_TRACE(...) Logger::getInstance().logPrintf(spdlog::level::trace, __VA_ARGS__)
 
 #define LOG_FATAL_STREAM Logger::getInstance().getLogger()->critical
 #define LOG_ERROR_STREAM Logger::getInstance().getLogger()->error
